ffed40c20ceaceff0b3377a8c2db84b646491b43
Idris Raja <idris.raja@gmail.com>
2011-10-09 23:23:00 -0700

    Project Euler problem in R and Python

---
+++ 1.py
@@ -0,0 +1,15 @@
+# If we list all the natural numbers below 10 that are multiples
+# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+import numpy as np
+import math
+
+total = 0
+for x in xrange(0, 1000):
+	if math.fmod(x, 3) == 0 or math.fmod(x, 5) == 0:
+		total += x
+print total
+ 
+
+
